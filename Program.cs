b2d029852c87d839e7eb3f89cf1e43a44ce9315d
Kim Langvannskås <Kimlangv@hotmail.com>
2024-05-09 21:34:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Opc.UaFx.Client;
+
+namespace DataLogger
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name the log file: ");
+            string path = Console.ReadLine() + ".csv";
+            Console.WriteLine("Storing data in "+path);
+
+            WriteCsvHeader(path);
+
+            using (var client = new OpcClient("opc.tcp://localhost:4840/")) //"opc.tcp://localhost:4841"
+            {
+                client.Connect();
+                while (true)
+                {
+                    var temperature = client.ReadNode("ns=2;s=Temperature");
+                    var heat = client.ReadNode("ns=2;s=Heat");
+                    var fan = client.ReadNode("ns=2;s=Fan");
+                    Console.WriteLine("Current Temperature is {0}°C, Heat is {1}v, Fan is {2}v", temperature, heat, fan);
+
+                    LogValuesToCsv(path, temperature, heat, fan);
+
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        static void WriteCsvHeader(string filePath)
+        {
+            // Write header to CSV file if the file doesn't exist
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    writer.WriteLine("Timestamp;Temperature (°C);Heat (V);Fan (V)");
+                }
+            }
+        }
+
+        static void LogValuesToCsv(string filePath, object temperature, object heat, object fan)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff");
+            // Log values to CSV file
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{timestamp};{temperature};{heat};{fan}");
+            }
+        }
+
+    }
+}
