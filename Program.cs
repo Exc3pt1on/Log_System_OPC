a71ec8676df9ac1a8cfaea100321b9bda17b05eb
Kim Langvannskås <Kimlangv@hotmail.com>
2024-05-15 11:48:05 +0200

    made function for naming path

--- Program.cs
+++ Program.cs
@@ -15,26 +15,34 @@ namespace DataLogger
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Name the log file: ");
-            string path = Console.ReadLine() + ".csv";
-            Console.WriteLine("Storing data in " + path);
-
-            WriteCsvHeader(path);
 
             while (true)
             {
                 try
                 {
+                    string path = NameFile();
                     Connect(path);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid path name...");
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("Connection lost, trying to reconnect...");
+                    throw;
                 }
             }
             
         }
-
+        static string NameFile()
+        {
+            Console.WriteLine("Name the log file: ");
+            string path = "Logs/" + Console.ReadLine() + ".csv";
+            WriteCsvHeader(path);
+            Console.WriteLine("Storing data in " + path);
+            return path;
+        }
         static void Connect(string path)
         {
             using (var client = new OpcClient("opc.tcp://localhost:4840/")) //"opc.tcp://localhost:4841"
@@ -47,14 +55,17 @@ namespace DataLogger
                     var fan = client.ReadNode("ns=2;s=Fan");
                     Console.WriteLine("Current Temperature is {0}°C, Heat is {1}v, Fan is {2}v", temperature, heat, fan);
 
+
+
                     if (path == ".csv")
                     {
-                        Thread.Sleep(1000);
+                        // Use "" in filename to not store to file
+                        Thread.Sleep(2000);
                     }
                     else
                     {
-                        LogValuesToCsv(path, temperature, heat, fan);
-                        Thread.Sleep(100);
+                        LogValuesToCsv(path, temperature.ToString(), heat.ToString(), fan.ToString());
+                        Thread.Sleep(500);
                     }
 
                 }
@@ -74,7 +85,7 @@ namespace DataLogger
 
         static void LogValuesToCsv(string filePath, object temperature, object heat, object fan)
         {
-            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff");
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");//.ff
             // Log values to CSV file
             using (StreamWriter writer = new StreamWriter(filePath, true))
             {
